f44e5efaf7a40148abdb425050e6998475427d80
nathan <radicalzer@gmail.com>
2023-01-14 12:51:01 -0500

    never commited

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
+let weeklyExpensesQuestions = []
+let monthlyExpensesQuestons = []
+let annualExpensesQuestions = []
 
 // Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
